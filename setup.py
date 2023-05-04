695626b3119d55cf1eda0bf9726f5936e73d848f
PaulineRollier <pauline.rollier@alten.com>
2023-05-04 15:24:16 +0200

    Ajout code + GitHubActions

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+import mathematique
+
+setup(
+    name="pkg_test_mathematique",
+    version=mathematique.__version__,
+    classifiers=["Topic :: Education", "Topic :: Documentation"],
+    packages=["mathematique", "test_mathematique"],
+    description="demonstration de creation d'un package Python",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.txt")).read(),
+    license="GPL V3",
+    platforms="ALL",
+)
