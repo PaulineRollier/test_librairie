3f413f477c0a951be843e7f6dbd41b83a86916f4
PaulineRollier <pauline.rollier@alten.com>
2023-05-23 13:27:45 +0200

    Changement dans setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import mathematique
 
 setup(
     name="pkg_test_mathematique_testpi",
-    version=mathematique.__version__,
+    version="0.0.3",
     packages=["mathematique"],
     description="demonstration de creation d'un package Python",
     long_description=open(os.path.join(os.path.dirname(__file__), "README.txt")).read(),
