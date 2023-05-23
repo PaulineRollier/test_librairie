dcaea061d73ed65901618b0f0e323480a23a0bc2
PaulineRollier <pauline.rollier@alten.com>
2023-05-23 11:39:16 +0200

    Retire release des yml

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 import mathematique
 
 setup(
     name="pkg_test_mathematique_testpi",
     version=mathematique.__version__,
-    classifiers=["Topic :: Education", "Topic :: Documentation"],
     packages=["mathematique"],
     description="demonstration de creation d'un package Python",
     long_description=open(os.path.join(os.path.dirname(__file__), "README.txt")).read(),
-    license="GPL V3",
-    platforms="ALL",
+    install_requires=["pytest", "sphinx", "sphinx_rtd_theme", "ghp-import"],
 )
