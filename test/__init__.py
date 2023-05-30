ecbe6aa9486f903151dd414b55a21472a38c6560
PaulineRollier <pauline.rollier@alten.com>
2023-05-30 12:08:47 +0200

    Nouveau yml pour tester le paquet compilé

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,8 @@
 import os
 import sys
 
-my_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, my_path + "/../")
+if not os.getenv("COMPILED_LIB_TEST", False) == "true":
+    my_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, my_path + "/../")
+
+print("Path : " + str(sys.path))
