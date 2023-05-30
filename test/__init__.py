ea2a5341de2b1c20d77c0e33e01a527b0c3fc6c1
PaulineRollier <pauline.rollier@alten.com>
2023-05-30 14:44:30 +0200

    Ajout echo path

--- test/__init__.py
+++ test/__init__.py
@@ -4,5 +4,3 @@ import sys
 if not os.getenv("COMPILED_LIB_TEST", False) == "true":
     my_path = os.path.dirname(os.path.abspath(__file__))
     sys.path.insert(0, my_path + "/../")
-
-print("Path : " + str(sys.path))
