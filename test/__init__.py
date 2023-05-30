7d10f4061d76e98771beb3264132b7952bab308f
PaulineRollier <pauline.rollier@alten.com>
2023-05-30 15:56:55 +0200

    Enlèvement de la variable d'environnement

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,5 @@
 import os
 import sys
 
-if not os.getenv("COMPILED_LIB_TEST", False) == "true":
-    my_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, my_path + "/../")
+my_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, my_path + "/../")
